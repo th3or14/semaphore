ed04e5be7bfc261b79a7674da3dc51e33e97ac32
th3or14 <th3or14@gmail.com>
2020-07-31 01:32:14 +0300

    add warning

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    // if this delay is not sufficient then some newer thread may acuqire a semaphore before
+    // another thread created earlier which results in race condition and fariness check violation
     static const std::chrono::milliseconds delay_between_threads_creation = 100ms;
     static const int fairness_check_threads_cnt = 10;
     std::cout << (run_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation) ?
