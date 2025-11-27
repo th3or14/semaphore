15344f661fb9f417a7e4274f393f82c6ad74e51a
th3or14 <37025359+th3or14@users.noreply.github.com>
2025-11-27 06:13:03 +0200

    Add failure points in tests

--- main.cpp
+++ main.cpp
@@ -8,23 +8,34 @@ int main()
     static const int fairness_check_threads_cnt = 10;
     static const int performance_benchmark_threads_cnt = 1000;
     std::cout << "proposed semaphore (ticket lock algorithm, queue of condition variables) tests:\n";
-    std::cout << (run_proposed_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation) ?
-                      "\t1) fairness check passed\n" :
-                      "\t1) fairness check failed\n");
+    if (run_proposed_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation))
+        std::cout << "\t1) fairness check passed\n";
+    else
+    {
+        std::cout << "\t1) fairness check failed\n";
+        return EXIT_FAILURE;
+    }
     std::cout << "\t2) benchmark took " <<
                  run_proposed_impl_performance_benchmark(
                      performance_benchmark_threads_cnt).count() << "ms\n";
     std::cout << "alternative semaphore (ticket lock algorithm, single condition variable) tests:\n";
-    std::cout << (run_alternative_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation) ?
-                      "\t1) fairness check passed\n" :
-                      "\t1) fairness check failed\n");
+    if (run_alternative_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation))
+        std::cout << "\t1) fairness check passed\n";
+    else
+    {
+        std::cout << "\t1) fairness check failed\n";
+        return EXIT_FAILURE;
+    }
     std::cout << "\t2) benchmark took " <<
                  run_alternative_impl_performance_benchmark(
                      performance_benchmark_threads_cnt).count() << "ms\n";
     std::cout << "unfair semaphore (no ticket lock algorithm) tests:\n";
-    std::cout << (run_unfair_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation) ?
-                      "\t1) fairness check passed\n" :
-                      "\t1) fairness check failed (expectedly for unfair semaphore)\n");
+    if (run_unfair_impl_fairness_check(fairness_check_threads_cnt, delay_between_threads_creation))
+        std::cout << "\t1) fairness check passed\n";
+    else
+    {
+        std::cout << "\t1) fairness check failed (expectedly for unfair semaphore)\n";
+    }
     std::cout << "\t2) benchmark took " <<
                  run_unfair_impl_performance_benchmark(
                      performance_benchmark_threads_cnt).count() << "ms\n";
